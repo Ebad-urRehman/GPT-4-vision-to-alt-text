db793b7b6f97ec239488b4455b4082e5435ecd00
Ibad-ur-Rehman <ibadkps@gmail.com>
2023-11-19 18:05:40 +0500

    func deleted pages 1

--- Home.py
+++ Home.py
@@ -9,6 +9,54 @@ import os
 import requests
 from pathlib import Path
 
+
+# ------------------------------------------FUNCTIONS CODE-------------------------------------
+
+class Chatbot:
+    def get_seo_optimized_words(self, messages):
+        try:
+            my_api_key = os.getenv("OPEN AI KEY")
+            client = openai.OpenAI(api_key=my_api_key)
+            response = client.chat.completions.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                max_tokens=300,
+            )
+            return response.choices[0].message.content
+
+        except Exception as e:
+            # Handle the exception here
+            st.info(f"An error occurred: Make sure that all urls in text file are valid image urls")
+            st.warning(f"ERROR Details are \n: {e}")
+            return None
+
+
+def is_valid_image_url(url):
+    try:
+        response = requests.head(url)
+        content_type = response.headers.get('content-type')
+        if content_type:
+            return True
+        else:
+            return False
+    except requests.RequestException:
+        return False
+
+def get_desktop_path():
+    # Get the user's home directory
+    home_dir = Path.home()
+
+    # Define the "Desktop" directory based on the operating system
+    desktop_dir = None
+    if os.name == 'posix':
+        desktop_dir = home_dir / 'Desktop'
+    elif os.name == 'nt':
+        desktop_dir = home_dir / 'Desktop'
+
+    return desktop_dir
+
+# -----------------------------Main File Code----------------------------
+
 # finding current date and time
 current_time = time.localtime()
 day = time.strftime("%d", current_time)
@@ -79,7 +127,7 @@ if txt_file:
         # appending new links to message dictionary so that can be send to gpt-4-vision-model
         for url in url_list:
             # this functions uses requests library to check if image urls are valid or not
-            check_image = functions.is_valid_image_url(url)
+            check_image = is_valid_image_url(url)
             if check_image:
                 new_dict ={
                     "type": "image_url",
@@ -131,51 +179,6 @@ if txt_file:
 
 
 
-# ------------------------------------------FUNCTIONS CODE-------------------------------------
-
-class Chatbot:
-    def get_seo_optimized_words(self, messages):
-        try:
-            my_api_key = os.getenv("OPEN AI KEY")
-            client = openai.OpenAI(api_key=my_api_key)
-            response = client.chat.completions.create(
-                model="gpt-4-vision-preview",
-                messages=messages,
-                max_tokens=300,
-            )
-            return response.choices[0].message.content
-
-        except Exception as e:
-            # Handle the exception here
-            st.info(f"An error occurred: Make sure that all urls in text file are valid image urls")
-            st.warning(f"ERROR Details are \n: {e}")
-            return None
 
 
-def is_valid_image_url(url):
-    try:
-        response = requests.head(url)
-        content_type = response.headers.get('content-type')
-        if content_type:
-            return True
-        else:
-            return False
-    except requests.RequestException:
-        return False
-
-def get_desktop_path():
-    # Get the user's home directory
-    home_dir = Path.home()
-
-    # Define the "Desktop" directory based on the operating system
-    desktop_dir = None
-    if os.name == 'posix':
-        desktop_dir = home_dir / 'Desktop'
-    elif os.name == 'nt':
-        desktop_dir = home_dir / 'Desktop'
-
-    return desktop_dir
-
-if __name__ == "__main__":
-    my_chatbot = Chatbot()
 
